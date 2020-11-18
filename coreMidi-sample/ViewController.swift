da1d6357a1bf8a8912960574b64b60bd276bc45f
A.Rabbit <acidrabbit0@gmail.com>
2020-11-19 02:11:35 +0900

    Initial Commit

---
+++ coreMidi-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreMidi-sample
+//
+//  Created by 服部翼 on 2020/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
